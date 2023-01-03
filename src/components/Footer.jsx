902d304d8a6a3965a914431756336792abc26ae3
finnius25 <finneykoshy@gmail.com>
2023-01-03 03:54:27 -0600

    footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Footer = () => {
+  return (
+    <div className="w-full h-28 bg-black text-white mt-20 flex items-center justify-center space-x-14">
+      <div>Designed & Built By Finney Koshy</div>
+      <div className="flex flex-col space-y-2 font-semibold">
+        <Link to="/">Portfolio</Link>
+        <Link to="About">About Me</Link>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -3,6 +3,7 @@ import Header from "../components/Header";
 import Hero from "../components/Hero";
 import Projects from "../components/Projects";
 import Collaborate from "../components/Collaborate";
+import Footer from "../components/Footer";
 
 const Portfolio = () => {
   return (
@@ -11,6 +12,7 @@ const Portfolio = () => {
       <Hero />
       <Projects />
       <Collaborate />
+      <Footer />
     </div>
   );
 };
