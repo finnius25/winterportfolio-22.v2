2ae00913d7c02a505349a4122d7ba67b57264d86
finnius25 <finneykoshy@gmail.com>
2023-01-14 12:17:46 -0600

    reworked layout

--- src/components/CollaborateAbout.jsx
+++ src/components/CollaborateAbout.jsx
@@ -4,38 +4,40 @@ import twitterIcon from "../assets/icons/icons8-twitter.svg";
 
 const CollaborateAbout = () => {
   return (
-    <div className="pt-10 px-5 space-y-10 lg:px-40 xl:px-60">
-      <div className="font-open font-semibold text-xl">
-        Would love to talk more, but I gotta bounce. Hit me up{" "}
-      </div>
-      <div>
-        <h2 className="font-lato font-semibold text-3xl">
-          finneykoshy@gmail.com
-        </h2>
-        <div className="flex space-x-3">
-          <div className="w-8 h-8 m-1">
-            <a
-              href="https://www.linkedin.com/in/finney-koshy-73913b248"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <img src={linkedInIcon} alt="LinkedIn Icon" />
-            </a>
-          </div>
-          <div className="w-8 h-8 m-1 mt-2">
-            <a
-              href="https://twitter.com/Finnius25"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <img src={twitterIcon} alt="Twitter Icon" />
-            </a>
-          </div>
+    <div className="w-full flex items-center justify-center mt-20">
+      <div className="w-full md:w-11/12 lg:w-9/12 lg:pl-5">
+        <div className="font-open font-semibold text-xl">
+          Would love to talk more, but I gotta bounce. Hit me up{" "}
         </div>
-        <p className="font-open font-light text-xl pt-5">
-          Designer & Developer
-        </p>
-        <p className="font-open font-light text-xl">Dallas, Tx</p>
+        <div>
+          <h2 className="font-lato font-semibold text-3xl">
+            finneykoshy@gmail.com
+          </h2>
+          <div className="flex space-x-3">
+            <div className="w-8 h-8 m-1">
+              <a
+                href="https://www.linkedin.com/in/finney-koshy-73913b248"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img src={linkedInIcon} alt="LinkedIn Icon" />
+              </a>
+            </div>
+            <div className="w-8 h-8 m-1 mt-2">
+              <a
+                href="https://twitter.com/Finnius25"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img src={twitterIcon} alt="Twitter Icon" />
+              </a>
+            </div>
+          </div>
+          <p className="font-open font-light text-xl pt-5">
+            Designer & Developer
+          </p>
+          <p className="font-open font-light text-xl">Dallas, Tx</p>
+        </div>
       </div>
     </div>
   );
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -6,7 +6,7 @@ const Projects = () => {
     <div className="w-screen h-screen flex flex-col items-center lg:justify-center px-5">
       <div className="flex flex-col w-full md:w-4/6 lg:w-5/6 space-y-20">
         <div className=" flex space-x-4 items-center">
-          <div className="bg-black h-0.5 w-5"></div>
+          <div className="bg-black h-8 w-0.5"></div>
           <h2 className="text-md py-2 font-lato font-normal">PROJECTS</h2>
         </div>
         <div className="flex flex-col space-y-20 xl:flex-row xl:space-x-10 xl:space-y-0 w-full ">
