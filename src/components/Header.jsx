a86fd8238ca0077767f8bc56159b66d90a94994c
finnius25 <finneykoshy@gmail.com>
2023-03-08 00:45:36 -0600

    hero & header animations

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,24 +1,37 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import arrowIcon from "../assets/icons/arrow.svg";
+import { motion } from "framer-motion";
 
 const Header = () => {
   return (
     <div className="w-full px-10 py-5 flex justify-center font-lato font-normal">
       <div className="w-11/12 flex justify-between">
-        <div>
+        <motion.div
+          whileHover={{
+            scale: 1.1,
+            transition: { duration: 0.3 },
+          }}
+          whileTap={{ scale: 0.9 }}
+        >
           <Link to="/">
             <h1 className="underline decoration-2 underline-offset-8 text-3xl">
               F
             </h1>
           </Link>
-        </div>
-        <div>
+        </motion.div>
+        <motion.div
+          whileHover={{
+            scale: 1.1,
+            transition: { duration: 0.3 },
+          }}
+          whileTap={{ scale: 0.9 }}
+        >
           <Link to="/About" className="flex items-center">
             <h1 className="text-xl pt-2">Me</h1>
             <img src={arrowIcon} alt="" className="w-6 h-6 mt-2" />
           </Link>
-        </div>
+        </motion.div>
       </div>
     </div>
   );
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import heroimg from "../assets/heroimg.png";
+import { motion } from "framer-motion";
 
 const Hero = () => {
   return (
@@ -8,14 +9,27 @@ const Hero = () => {
         <img src={heroimg} alt="" className="w-full h-full object-contain" />
       </div>
       <div className="lg:m-10 flex flex-col font-open space-y-5 w-5/6 md:w-4/6 lg:w-6/12">
-        <h1 className="text-5xl leading-normal font-semibold">
-          Hi, I’m Finney Koshy 👋
-        </h1>
-        <p className="text-xl font-light leading-loose md:w-8/12 ">
-          <em>Exploring</em> aesthetic designs, <em>Constructing</em> creative
-          solutions, and <em>Dabbling</em> in entrepreneurialism. Based in
-          Dallas, TX. Currenltly building <strong>Sterling</strong>.
-        </p>
+        <motion.div
+          initial={{ x: -60, opacity: 0 }}
+          animate={{ x: 0, opacity: 1 }}
+          transition={{ delay: 0.6, duration: 0.9 }}
+        >
+          <h1 className="text-5xl leading-normal font-semibold">
+            Hi, I’m Finney Koshy 👋
+          </h1>
+        </motion.div>
+
+        <motion.div
+          initial={{ x: -60, opacity: 0 }}
+          animate={{ x: 0, opacity: 1 }}
+          transition={{ delay: 0.7, duration: 0.8 }}
+        >
+          <p className="text-xl font-light leading-loose md:w-8/12 ">
+            <em>Exploring</em> aesthetic designs, <em>Constructing</em> creative
+            solutions, and <em>Dabbling</em> in entrepreneurialism. Based in
+            Dallas, TX. Currenltly building <strong>Sterling</strong>.
+          </p>
+        </motion.div>
       </div>
     </div>
   );
